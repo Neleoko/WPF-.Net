0e83f274e21ab7b0aa74ac534f0330b8d77a57f6
Neleoko <adda.ephraim@gmail.com>
2024-11-06 16:39:30 +0100

    init

---
+++ App.xaml.cs
@@ -0,0 +1,1 @@
+﻿using System.Configuration;using System.Data;using System.Windows;namespace WPF_.Net;/// <summary>/// Interaction logic for App.xaml/// </summary>public partial class App : Application{}
