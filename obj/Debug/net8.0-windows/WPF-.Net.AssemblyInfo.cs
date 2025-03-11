de7faa2316ad31532ffe117c7c9e1be86e8667f4
Ephraim Adda <adda.ephraim@gmail.com>
2025-03-11 09:15:42 +0100

    fix styles path

--- obj/Debug/net8.0-windows/WPF-.Net.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WPF-.Net.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF-.Net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cbb145d05ded80f3d26c9d0553e75b2d3368cae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ec657caf924e347eab639a2f323544c6723d14")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF-.Net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF-.Net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
